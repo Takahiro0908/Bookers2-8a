25623455244a83a78c8f3c28a3d8e56f15d15f42
EC2 Default User <ec2-user@ip-172-31-5-201.ec2.internal>
2022-06-04 12:54:20 +0000

    [Finish]Bookers2-6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,16 +2,20 @@ class BookCommentsController < ApplicationController
   before_action :authenticate_user!
   
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    render :book_comments #render先にjsファイルを指定
+    # redirect_to request.referer　＃非同期通信のため削除
   end
   
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    #renderしたときに@bookのデータがないので@bookを定義
+    @book = Book.find(params[:book_id])
+    render :book_comments #render先にjsファイルを指定
+    # redirect_to request.referer
   end
 
   private
